b1f51e5eef8d5b08505100e17a6dd0decbd0d5bc
Sam Wang <sevenbamboos@gmail.com>
2016-05-27 06:44:02 +0800

    init asp.net mvc 4

---
+++ CalculatorWeb2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using samw.Calculator.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculatorWeb2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            Exam exam = new Exam();
+            exam.Add(new Exercise("Add I", Expression.InitAdd, 10, 10, 10));
+            StringBuilder sb = new StringBuilder();
+            foreach (IEvaluable eva in exam.Generate(false))
+            {
+                sb.AppendFormat("{0},", eva);
+            }
+            ViewBag.Message = sb.ToString();
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CalculatorWeb2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CalculatorWeb2.Startup))]
+namespace CalculatorWeb2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
